cd94f364958f98dd2da528c1ec0bcbe83a631286
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-06-02 01:30:22 +0530

    Time: 63 ms (80.12%), Space: 40.9 MB (34.44%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -1,27 +1,61 @@
+#include<bits/stdc++.h>
+
 class Solution {
 public: 
 
-    // Brute Force O(N^3)......................................
+    // OPTIMIZED SOLUTION O(N^2)..................................
     int equalPairs(vector<vector<int>>& grid) {
-        int n = grid.size(); 
+        map<vector<int>, int> mp; 
+        int n = grid.size() ;
         int m = grid[0].size(); 
         int count = 0 ;
+
+        
         for(int r=0; r<n; r++){
+            vector<int> temp ;
             for(int c=0; c<m; c++){
-                bool isEqual = true ; 
+                temp.push_back(grid[r][c]); 
+            }
 
-                for(int i=0; i<n; i++){
-                    if(grid[r][i] != grid[i][c]){
-                        isEqual = false; 
-                        break ;
-                    }                                 
-                }
+            mp[temp]++ ;
+        }
 
-                if(isEqual) count++ ;
+        
+        for(int c=0; c<m; c++){
+            vector<int> temp2 ;
+            for(int r=0; r<n; r++){
+                temp2.push_back(grid[r][c]); 
+            }
 
+            if(mp.find(temp2) != mp.end()){
+                count += mp[temp2] ;
             }
         }
 
         return count ;
     }
+
+    // // Brute Force O(N^3)......................................
+    // int equalPairs(vector<vector<int>>& grid) {
+    //     int n = grid.size(); 
+    //     int m = grid[0].size(); 
+    //     int count = 0 ;
+    //     for(int r=0; r<n; r++){
+    //         for(int c=0; c<m; c++){
+    //             bool isEqual = true ; 
+
+    //             for(int i=0; i<n; i++){
+    //                 if(grid[r][i] != grid[i][c]){
+    //                     isEqual = false; 
+    //                     break ;
+    //                 }                                 
+    //             }
+
+    //             if(isEqual) count++ ;
+
+    //         }
+    //     }
+
+    //     return count ;
+    // }
 };
