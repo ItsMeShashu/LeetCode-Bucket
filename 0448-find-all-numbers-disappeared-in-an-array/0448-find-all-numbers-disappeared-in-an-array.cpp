5d6b657aebad88832b962d2fa4cfb16b6cb20295
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-08-06 15:07:55 +0530

    Time: 31 ms (91.01%), Space: 36.3 MB (59.42%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
+    //........................O(n) extra
+    //space.....................................
+    // vector<int> findDisappearedNumbers(vector<int>& nums) {
+    //     int n = nums.size() ;
+    //     unordered_map<int , int> mp;
+    //     vector<int> ans ;
+    //     for(int i = 0; i<n ; i++){
+    //         mp[nums[i]]++ ;
+    //     }
+    //     for(int i=1; i<=n; i++){
+    //         if(mp[i] == 0) ans.push_back(i) ;
+    //     }
+    //     return ans;
+    // }
+
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size() ; 
-        unordered_map<int , int> mp; 
-        vector<int> ans ; 
-
-        for(int i = 0; i<n ; i++){
-            mp[nums[i]]++ ;
+        for (auto num : nums) {
+            int i = abs(num) - 1;
+            nums[i] = -1 * abs(nums[i]);
         }
-
-        for(int i=1; i<=n; i++){
-            if(mp[i] == 0) ans.push_back(i) ; 
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) ans.push_back(i + 1);
         }
         return ans;
     }
