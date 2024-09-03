e17c032090a8adcbab134da4b9441f8de9978bb0
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-09-03 10:02:42 +0530

    Time: 8 ms (61.74%), Space: 9.8 MB (18.47%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int i = 0, j = 0 ;
-        int maxFreq = INT_MIN ; 
-        int maxLen = 0 ;
-        unordered_map<char, int> mp; 
-
-        int n = s.length() ; 
+        unordered_map<char, int> mp ;
+        int n = s.length() ;
+        int i = 0, j=0; 
+        int maxLen = 0;
+        int maxFreq = INT_MIN;
         while(j<n){
-            mp[s[j]]++ ; 
-            maxFreq = max(maxFreq, mp[s[j]]) ; 
-            int windowSize = j-i+1 ;  
-            int minElemToChange = windowSize - maxFreq ; 
+            mp[s[j]]++ ;
+            maxFreq = max(maxFreq, mp[s[j]]); 
 
-            if(minElemToChange > k){
-                mp[s[i]]-- ; 
-                i++ ; 
+            int windowSize = j-i+1; 
+            int elemsToChange = windowSize - maxFreq ; //No. of elemnts which need to change in that window
+
+            if(elemsToChange > k){
+                mp[s[i]]-- ;
+                i++ ;
             }
+            int newWindow = j-i+1 ;
+            maxLen = max(maxLen, newWindow) ;
+            j++ ;
+        } 
 
-            int newWindow = j-i+1 ; 
-            maxLen = max(maxLen, newWindow) ; 
-            j++ ; 
-        }
-
-        return maxLen ; 
+        return maxLen; 
     }
 };
