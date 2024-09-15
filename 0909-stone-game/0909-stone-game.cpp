ec1bef6f549b71e5a8ba887087a7e527bfd3251a
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-09-15 13:14:21 +0530

    Time: 14 ms (22.85%), Space: 18.6 MB (21.35%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+
+    int memFunc(vector<int>& piles, vector<vector<int>> &dp, int s, int e){
+        if(s >= e){
+            return 0; 
+        }
+
+        if(dp[s][e] != -1) return dp[s][e] ; 
+
+        dp[s][e] = max(piles[s] - memFunc(piles, dp, s+1, e), piles[e] - memFunc(piles, dp, s, e-1)) ; 
+
+        return dp[s][e] ; 
+    }
+
+
     bool stoneGame(vector<int>& piles) {
-        return true ; 
+        int n = piles.size();  
+        vector<vector<int>> dp(n, vector<int>(n, -1)) ; 
+
+        return memFunc(piles, dp, 0, n-1) != 0 ; 
     }
 };
