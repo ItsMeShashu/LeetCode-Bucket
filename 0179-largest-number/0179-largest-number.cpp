a4b44042be7abf3e9a6573b5bd486d1b02c5517d
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-09-18 21:25:41 +0530

    Time: 7 ms (61.31%), Space: 15.7 MB (70.19%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
 
-    static bool cmp(int a, int b){
-        string first = to_string(a) + to_string(b);
-        string second = to_string(b) + to_string(a);
-        return first>second;
-    } 
+    // static bool cmp(int a, int b){
+    //     string first = to_string(a) + to_string(b);
+    //     string second = to_string(b) + to_string(a);
+    //     return first>second;
+    // } 
 
     string largestNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),cmp);
+        sort(nums.begin(),nums.end(), [](int a, int b){
+            string first = to_string(a) + to_string(b);
+            string second = to_string(b) + to_string(a);
+            return first>second;
+        });
         string ans="";
         
         for(int i=0;i<nums.size();i++) 
