47e8e29166bd12aefb8310e9c5f8fd620d1d2e3c
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-09-26 11:43:12 +0530

    Time: 77 ms (66.21%), Space: 41.7 MB (86.63%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,15 @@
+class MyCalendar {
+public:
+    vector<pair<int, int>> ranges;
+    MyCalendar() {}
+    bool book(int start, int end) {
+        for (auto pair : ranges) {
+            int r_start = pair.first;
+            int r_end = pair.second;
+            if (start < r_end && end > r_start)
+                return false;
+        }
+        ranges.push_back(make_pair(start, end));
+        return true;
+    }
+};
