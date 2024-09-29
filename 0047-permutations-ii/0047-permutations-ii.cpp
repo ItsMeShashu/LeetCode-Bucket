2e1580a14b870ce9eb50a12affb9d3306793466c
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-09-29 12:24:44 +0530

    Time: 122 ms (8.46%), Space: 42.8 MB (6.11%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -30,32 +30,3 @@ public:
         return result;
     }
 };
-
-    // void solve(vector<int> nums, int i, set<vector<int>> &st) {
-    //     if (i >= nums.size()) {
-    //         st.insert(nums);
-    //         return;
-    //     }
-        
-    //     for (int j = i; j < nums.size(); j++) {
-    //         swap(nums[j], nums[i]);
-
-    //         solve(nums, i + 1, st);
-
-    //         swap(nums[j], nums[i]);
-    //     }
-    // }
-
-    // vector<vector<int>> permute(vector<int>& nums) {
-    //     vector<vector<int>> result;
-    //     set<vector<int>> st ;
-
-    //     int i = 0;
-    //     solve(result, nums, i, st);
-
-    //     for(auto vec: st){
-    //         result.push_back(vec) ; 
-    //     }
-
-    //     return result;
-    // }
