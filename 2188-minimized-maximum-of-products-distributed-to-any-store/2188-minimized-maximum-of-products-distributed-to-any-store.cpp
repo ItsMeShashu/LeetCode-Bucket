b36d20433d622fc1ec81555ceb9e60e41b4ef288
Shashwat Garg <111221384+ItsMeShashu@users.noreply.github.com>
2024-11-14 09:59:35 +0530

    Time: 39 ms (62.92%), Space: 87.3 MB (34.13%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int s = 1, e = *max_element(quantities.begin(), quantities.end());
+        while(s < e){
+            int mid = s + (e-s)/2;
+            int total = 0;
+            for(int quantity:quantities){
+                total += (quantity+mid-1)/mid;
+            }
+            if(total <= n){
+                e = mid;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return s; 
+    }
+};
